b46ca5b00cd54886e533d6d9a5520b4bb209a493
Charles Wang <zwcharl@gmail.com>
2026-02-08 05:50:37 -0500

    feat: imgui demo window + webgpu render loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include "SDL3/SDL_error.h"
+#include "SDL3/SDL_video.h"
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <imgui.h>
@@ -8,8 +11,10 @@
 
 #include <cstdint>
 #include <cstdlib>
+#include <exception>
 #include <limits>
 #include <print>
+#include <utility>
 
 namespace {
 
@@ -18,8 +23,8 @@ constexpr uint64_t WAIT_TIMEOUT_MAX = std::numeric_limits<uint64_t>::max();
 
 constexpr auto SDL_INIT_FLAGS = SDL_INIT_VIDEO;
 
-constexpr int SCREEN_W = 640;
-constexpr int SCREEN_H = 480;
+constexpr int SCREEN_W = 1280;
+constexpr int SCREEN_H = 720;
 
 }
 
@@ -140,12 +145,19 @@ int main(int, char*[])
     return EXIT_FAILURE;
   }
 
+  int width_in_pixels = -1;
+  int height_in_pixels = -1;
+  if (!SDL_GetWindowSizeInPixels(window, &width_in_pixels, &height_in_pixels)) {
+    std::println(stderr, "Failed to get SDL window size in pixels: {}", SDL_GetError());
+    return EXIT_FAILURE;
+  }
+
   wgpu::SurfaceConfiguration surface_configuration = {
     .device = device,
     .format = surface_capabilities.formats[0],
     .usage = wgpu::TextureUsage::RenderAttachment,
-    .width = SCREEN_W,
-    .height = SCREEN_H,
+    .width = static_cast<uint32_t>(width_in_pixels),
+    .height = static_cast<uint32_t>(height_in_pixels),
     .alphaMode = wgpu::CompositeAlphaMode::Auto,
     .presentMode = wgpu::PresentMode::Fifo,
   };
@@ -154,16 +166,102 @@ int main(int, char*[])
 
   wgpu::Queue queue = device.GetQueue();
 
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+
+  ImGuiIO& io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+  ImGui::StyleColorsDark();
+
+  ImGui_ImplSDL3_InitForOther(window);
+
+  ImGui_ImplWGPU_InitInfo imgui_wgpu_init_info;
+  imgui_wgpu_init_info.Device = device.Get();
+  imgui_wgpu_init_info.NumFramesInFlight = 3;
+  imgui_wgpu_init_info.RenderTargetFormat
+      = static_cast<WGPUTextureFormat>(surface_configuration.format);
+  imgui_wgpu_init_info.DepthStencilFormat = WGPUTextureFormat_Undefined;
+  ImGui_ImplWGPU_Init(&imgui_wgpu_init_info);
+
+  io.Fonts->AddFontDefault();
+
   bool will_quit = false;
   SDL_Event event = {};
 
   while (!will_quit) {
     while (SDL_PollEvent(&event)) {
+      ImGui_ImplSDL3_ProcessEvent(&event);
+
       if (event.type == SDL_EVENT_QUIT) {
+        will_quit = true;
+      } else if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED
+          && event.window.windowID == SDL_GetWindowID(window)) {
         will_quit = true;
       }
     }
+
+    wgpu::SurfaceTexture surface_texture;
+    surface.GetCurrentTexture(&surface_texture);
+
+    if (auto status = static_cast<WGPUSurfaceGetCurrentTextureStatus>(surface_texture.status);
+        ImGui_ImplWGPU_IsSurfaceStatusError(status)) {
+      std::println(
+          stderr, "Unrecoverable surface texture status: {:#x}", std::to_underlying(status));
+      std::terminate();
+    }
+
+    ImGui_ImplWGPU_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::ShowDemoWindow();
+
+    ImGui::Render();
+
+    wgpu::TextureViewDescriptor texture_view_desc = {
+      .format = surface_configuration.format,
+      .dimension = wgpu::TextureViewDimension::e2D,
+      .aspect = wgpu::TextureAspect::All,
+    };
+
+    wgpu::TextureView texture_view = surface_texture.texture.CreateView(&texture_view_desc);
+
+    wgpu::RenderPassColorAttachment color_attachment = {
+      .view = texture_view,
+      .loadOp = wgpu::LoadOp::Clear,
+      .storeOp = wgpu::StoreOp::Store,
+      .clearValue = wgpu::Color { .r = 1.0, .g = 0.0, .b = 0.0, .a = 1.0 },
+    };
+
+    wgpu::CommandEncoderDescriptor encoder_desc;
+    wgpu::CommandEncoder command_encoder = device.CreateCommandEncoder(&encoder_desc);
+
+    wgpu::RenderPassDescriptor render_pass_desc = {
+      .colorAttachmentCount = 1,
+      .colorAttachments = &color_attachment,
+    };
+
+    wgpu::RenderPassEncoder render_pass_encoder
+        = command_encoder.BeginRenderPass(&render_pass_desc);
+    ImGui_ImplWGPU_RenderDrawData(ImGui::GetDrawData(), render_pass_encoder.Get());
+    render_pass_encoder.End();
+
+    wgpu::CommandBufferDescriptor cmd_buf_desc;
+    wgpu::CommandBuffer cmd_buf = command_encoder.Finish(&cmd_buf_desc);
+
+    queue.Submit(1, &cmd_buf);
+
+    surface.Present();
+    device.Tick();
   }
+
+  ImGui_ImplWGPU_Shutdown();
+  ImGui_ImplSDL3_Shutdown();
+  ImGui::DestroyContext();
+
+  surface.Unconfigure();
 
   SDL_DestroyWindow(window);
   SDL_QuitSubSystem(SDL_INIT_FLAGS);
