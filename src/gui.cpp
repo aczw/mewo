0552deaf734542afbb71cb3985f4735b129f2e8f
Charles Wang <zwcharl@gmail.com>
2026-02-14 19:36:39 -0500

    feat: very basic imgui docking

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,11 +1,19 @@
 #include "gui.hpp"
 
-#include <imgui.h>
+#include "imgui.h"
+
 #include <imgui_impl_sdl3.h>
 #include <imgui_impl_wgpu.h>
+#include <imgui_internal.h>
 #include <webgpu/webgpu_cpp.h>
 
+#include <string_view>
+
 namespace mewo {
+
+static constexpr std::string_view MAIN_DOCKSPACE_STR_ID = "main-dockspace";
+static constexpr std::string_view EDITOR_WINDOW_NAME = "Editor";
+static constexpr std::string_view OUTPUT_WINDOW_NAME = "Output";
 
 Gui::Gui(const sdl::Window& window, const gfx::Renderer& renderer)
 {
@@ -26,6 +34,9 @@ Gui::Gui(const sdl::Window& window, const gfx::Renderer& renderer)
   ImGui_ImplWGPU_Init(&wgpu_init_info);
 
   ImGui_ImplSDL3_InitForOther(window.get());
+
+  // Can be set once upfront because there's only one viewport
+  viewport_ = ImGui::GetMainViewport();
 }
 
 Gui::~Gui()
@@ -41,10 +52,25 @@ void Gui::record(const gfx::FrameContext& frame_ctx) const
   ImGui_ImplSDL3_NewFrame();
   ImGui::NewFrame();
 
+  ImGuiID dockspace_id = ImGui::GetID(MAIN_DOCKSPACE_STR_ID.data());
+
+  // One time setup, but can only be done after a new frame is initiated
+  if (ImGui::DockBuilderGetNode(dockspace_id) == nullptr)
+    set_up_layout(dockspace_id);
+
+  // Submit dockspace layout
+  ImGui::DockSpaceOverViewport(dockspace_id, viewport_, ImGuiDockNodeFlags_PassthruCentralNode);
+
   {
-    // This is where I would actually create the UI but currently I'm just
-    // showing a demo window
-    ImGui::ShowDemoWindow();
+    ImGui::Begin(EDITOR_WINDOW_NAME.data());
+    ImGui::Text("I am the editor");
+    ImGui::End();
+  }
+
+  {
+    ImGui::Begin(OUTPUT_WINDOW_NAME.data());
+    ImGui::Text("I am the output");
+    ImGui::End();
   }
 
   ImGui::Render();
@@ -66,6 +92,21 @@ void Gui::record(const gfx::FrameContext& frame_ctx) const
   wgpu::RenderPassEncoder render_pass = encoder.BeginRenderPass(&render_pass_desc);
   ImGui_ImplWGPU_RenderDrawData(ImGui::GetDrawData(), render_pass.Get());
   render_pass.End();
+}
+
+void Gui::set_up_layout(ImGuiID dockspace_id) const
+{
+  ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_DockSpace);
+  ImGui::DockBuilderSetNodeSize(dockspace_id, viewport_->Size);
+
+  ImGuiID dock_left_id = {};
+  ImGuiID dock_right_id = {};
+  ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Left, 0.5f, &dock_left_id, &dock_right_id);
+
+  ImGui::DockBuilderDockWindow(EDITOR_WINDOW_NAME.data(), dock_left_id);
+  ImGui::DockBuilderDockWindow(OUTPUT_WINDOW_NAME.data(), dock_right_id);
+
+  ImGui::DockBuilderFinish(dockspace_id);
 }
 
 }
--- src/gui.hpp
+++ src/gui.hpp
@@ -4,10 +4,12 @@
 #include "gfx/renderer.hpp"
 #include "sdl/window.hpp"
 
+#include <imgui.h>
 #include <webgpu/webgpu_cpp.h>
 
 namespace mewo {
 
+/// Immediate mode GUI rendering using Dear ImGui.
 class Gui {
   public:
   Gui(const sdl::Window& window, const gfx::Renderer& renderer);
@@ -17,6 +19,11 @@ class Gui {
   Gui& operator=(const Gui&) = delete;
 
   void record(const gfx::FrameContext& frame_ctx) const;
+
+  private:
+  void set_up_layout(ImGuiID dockspace_id) const;
+
+  ImGuiViewport* viewport_ = nullptr;
 };
 
 }
